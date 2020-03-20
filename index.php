7df9625999a6db0ff8d82188f900830c304cdcbc
saiful355 <bahrisaif09@mail.com>
2020-03-21 02:34:58 +0700

    TUGAS 8(SAIFUL BAHRI)

--- index.php
+++ index.php
@@ -115,12 +115,39 @@ var_dump(1 == 2);
 	echo "</br>";
 
 var_dump(1 != 4);
+
+
+var_dump (3 === "3");
+
+ var_dump (4 === "4");
+
+ var_dump (5 === "5");
+
+ var_dump (6 === "6");
+
+ var_dump (7 === "7");
 	echo "</br>";
 
 	//oprator logika
 
 	$x = 10;
 	var_dump($x < 20 && $x % 2 == 0);
+
+	$x = 13;
+ var_dump($x < 7 && $x % 3 == 0);
+
+ $x = 4;
+ var_dump($x < 12 && $x % 4 == 0);
+
+ $x = 5;
+ var_dump($x < 1 && $x % 2 == 0);
+
+ $x = 23;
+ var_dump($x < 20 && $x % 12 == 0);
+
+ $x = 11;
+ var_dump($x < 5 && $x % 2 == 0);
+
 
 
 	?>				
