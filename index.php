84d19d82ecbd04ff837dec8d964f5fc8d73dee10
saiful355 <bahrisaif09@mail.com>
2020-03-21 02:12:54 +0700

    tugass2

--- index.php
+++ index.php
@@ -24,3 +24,25 @@ $nama = "saiful";
 
 	</body>
 	</html>		
+
+
+	<?
+
+$nama = "saiful";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		belajar php
+
+</title>
+</head>
+<body>
+
+	<h1>hello, selamat datang <?php echo $saiful;| ?></h1>
+
+	</body>
+	</html>		
+	
