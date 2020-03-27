b0397bcf911ba562c6793bae656a6ef93b1d8624
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-03-27 21:54:12 +0700

    Latihan 3

---
+++ Minggu4/Latihan3.php
@@ -0,0 +1,14 @@
+<?php
+// perulangan
+// while
+
+$i = 0; #bagian inisialisai
+while ($i < 5 ){ #didalam bracket () kondisi terminasi
+	
+	echo "Hai, Dunia! <br>";
+		$i++; #increment
+	
+}
+
+
+?>
