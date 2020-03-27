ac3a4890f40c54b18534142e51cfd7b4662d0452
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-03-27 21:48:12 +0700

    kontrol_flow.php

---
+++ Minggu4/kontrol_flow.php
@@ -0,0 +1,33 @@
+<!DOCTIPE html>
+ <html lang= "en">
+ <head>
+
+     <meta charset= "UTF-8">
+
+     <title>
+     	latihan perulang tabel
+     	</title>
+     		</head>
+     			<body>
+
+<h2>
+	Membuat perulangan tabel
+	  </h2>
+	  <table berder="1" cellpadding="20" cellspacing="0">
+	  	for ($b = 1; $b <= 20; $b++){
+	  	if ($b % 2 === 1 ){
+	  		echo "<tr bgcolor= '#ff9990>";
+	  	}
+	  	else 
+	  		echo "<tr>";
+
+
+	  		for ($k=1 <=9 ; $k++){
+	  			echo "<td> $b,$k </td>";
+	  		}
+
+	  		echo "</tr>";
+
+	  	}   			
+	  	?>
+	  	</table>
