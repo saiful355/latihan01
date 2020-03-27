348af99b9d6f425e23ea87f66e26d3c2011da906
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-03-27 21:22:19 +0700

    Hello Word Messege

---
+++ Minggu4/latihan.php
@@ -0,0 +1,14 @@
+<?php
+  // penggulangan
+  // for
+
+  for ($i = 0; $i < 5; i++){
+  	  echo "Hello Word!  <br>";
+  }
+
+  /* $i = 0; bagian inisialisasi / menentukan variabel
+  $1 < 5;   bagian kondisi/ untuk memberhentikan kondisi perulangan ketika berjumlah <5
+
+  i++     bagian increment (menambah) kondisi*/
+
+  ?>
