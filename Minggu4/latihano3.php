ffc460d0ca52053cb63b726f146a205b3e1c3f42
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-03-27 22:15:45 +0700

    latihan03

--- Minggu4/latihano3.php
+++ Minggu4/latihano3.php
@@ -4,6 +4,15 @@ if ($x < 20){#kondisi pengecekan dalam bracket
 	
 		echo "BENAR!"; Jika true ini yang dicetak
 
+
+			# code...
+		} elseif ($x == 20 ) { #kondisi tepat
+			echo "BINGGO!";
+			# code...
+		}
+
+		
+
 }	else {
 		echo "SALAH"; jika false ini yang dicetak
 }
