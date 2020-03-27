e337ed61e4fd63a0a7ae69c0a6dac70b3fc40425
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-03-27 22:27:36 +0700

    latihan04

---
+++ Minggu4/latihan4.php
@@ -0,0 +1,28 @@
+<?php
+$x =20;
+
+if ($x >= 90){
+	echo "A+";
+
+}elseif ($x >= 80 && $x < 90) {
+	echo "A";
+	# code...
+}elseif ($x >= 70 && $x < 80) {
+	echo "B";
+	# code...
+}elseif ($x >= 60 && $x < 70) {
+	echo "C";
+	# code...
+}elseif ($x >= 50 && $x < 60) {
+	echo "D";
+	# code...
+}elseif ($x >= 0 && $x < 50) {
+	echo "E";
+	# code...
+}
+{
+	echo "salah";
+
+}
+
+?>
