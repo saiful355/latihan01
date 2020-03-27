99a7337cb461d6ba8d8764a1ea6514ce9c881920
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-03-27 22:07:29 +0700

    latihan03

---
+++ Minggu4/latihano3.php
@@ -0,0 +1,11 @@
+<?php
+$x = 10;	#kondisi variabel
+if ($x < 20){#kondisi pengecekan dalam bracket
+	
+		echo "BENAR!"; Jika true ini yang dicetak
+
+}	else {
+		echo "SALAH"; jika false ini yang dicetak
+}
+
+?>
