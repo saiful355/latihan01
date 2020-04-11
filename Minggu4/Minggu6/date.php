4acb13959668ebeb3166e83ba3d95b5c367e72b7
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-04-11 19:15:39 +0700

    TASK_5

--- Minggu4/Minggu6/date.php
+++ Minggu4/Minggu6/date.php
@@ -3,16 +3,11 @@
 
 	echo date("1, d-M-Y",
 			
-			time()  +
-
-				60 * 60 * 24 * 100
-					time () +
-				60 * 60 * 24 *1000
-
-				time () -
-				60 * 60 * 24 * 500
+			time ()-
+			60 * 60 * 24 * 500
 		);
 
+			echo date('d-m-y',mktime(0,0,11,04,1992));
 
 	// dengan format tersebut kita dapat mengetahui 1000 hari yang akan datang hari apa
 
