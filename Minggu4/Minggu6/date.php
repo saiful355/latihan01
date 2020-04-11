e97c1386f3471999f7fb394146cbd126db9ba46b
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-04-11 18:45:36 +0700

    TASK_2

--- Minggu4/Minggu6/date.php
+++ Minggu4/Minggu6/date.php
@@ -1,7 +1,15 @@
 <?php
 //date untuk menampilkan tanggal format tertentu
 
-	echo date("1");
+	echo date("1, d-M-Y",
+			
+			time()  +
+
+				60 * 60 * 24 * 1000
+		);
+
+
+	// dengan format tersebut kita dapat mengetahui 1000 hari yang akan datang hari apa
 
 
 ?>	
