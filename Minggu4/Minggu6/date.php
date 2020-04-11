ee2ae736c1515c96e37fcc58857c76e90389a788
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-04-11 19:22:35 +0700

    TASK_8

--- Minggu4/Minggu6/date.php
+++ Minggu4/Minggu6/date.php
@@ -1,9 +1,9 @@
 <?php
-echo date("l", strtotime('30 August 2030'));
-?> 
 function salam($waktu, $nama)
+function perkalian($angka1, $angka2)
 {
 	return "Selamat $waktu, $nama!";
+	return $angka1*$angka2;
 }
 ?>
 <!DOCTYPE html>
@@ -19,3 +19,8 @@ function salam($waktu, $nama)
 </body>
 </!DOCTYPE html>
 <html> 
+$hasil = perkalian(20,3);
+echo "Perkalian 20 x 3 adalah $hasil";
+echo "<br>";
+echo "Perkalian 9 x 2 ".perkalian(9,2);
+?>
